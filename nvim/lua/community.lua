67207f212b57398fad6dee678673c565a3c9a831
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2024-09-03 17:29:08 -0600

    ref(nvim): move noice.nvim to it's file

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -15,59 +15,6 @@ return {
   { import = "astrocommunity.pack.cpp" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.typescript" },
-  { import = "astrocommunity.utility.noice-nvim" },
-  {
-    "folke/noice.nvim",
-    opts = {
-      messages = {
-        view = false,
-      },
-      lsp = {
-        hover = {
-          -- silent = true,
-        },
-        override = {
-          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-          ["vim.lsp.util.stylize_markdown"] = true,
-          ["cmp.entry.get_documentation"] = true,
-        },
-      },
-      routes = {
-        {
-          filter = {
-            event = "notify",
-            find = "No information available",
-          },
-          opts = { skip = true },
-        },
-        {
-          filter = {
-            event = "notify",
-            find = "Invalid offset LineCol",
-          },
-          opts = { skip = true },
-        },
-        {
-          filter = {
-            event = "msg_show",
-            any = {
-              { find = "%d+L, %d+B" },
-              { find = "; after #%d+" },
-              { find = "; before #%d+" },
-            },
-          },
-          view = "mini",
-        },
-      },
-      presets = {
-        bottom_search = true,
-        command_palette = true,
-        long_message_to_split = true,
-        inc_rename = true,
-        lsp_doc_border = true,
-      },
-    },
-  },
   { import = "astrocommunity.completion.copilot-lua" },
   {
     "zbirenbaum/copilot.lua",
@@ -93,6 +40,7 @@ return {
     },
   },
   { import = "astrocommunity.programming-language-support.csv-vim" },
+  { import = "astrocommunity.utility.noice-nvim" },
   { import = "astrocommunity.bars-and-lines.smartcolumn-nvim" },
   {
     "m4xshen/smartcolumn.nvim",
---
+++ nvim/lua/plugins/noice.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "folke/noice.nvim",
+    opts = {
+      messages = {
+        view = false,
+      },
+      lsp = {
+        hover = {
+          -- silent = true,
+        },
+        override = {
+          ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+          ["vim.lsp.util.stylize_markdown"] = true,
+          ["cmp.entry.get_documentation"] = true,
+        },
+      },
+      routes = {
+        {
+          filter = {
+            event = "notify",
+            find = "No information available",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "notify",
+            find = "Invalid offset LineCol",
+          },
+          opts = { skip = true },
+        },
+        {
+          filter = {
+            event = "msg_show",
+            any = {
+              { find = "%d+L, %d+B" },
+              { find = "; after #%d+" },
+              { find = "; before #%d+" },
+            },
+          },
+          view = "mini",
+        },
+      },
+      presets = {
+        bottom_search = true,
+        command_palette = true,
+        long_message_to_split = true,
+        inc_rename = true,
+        lsp_doc_border = true,
+      },
+    },
+  },
+}
