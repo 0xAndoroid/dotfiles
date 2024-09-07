7bea895d9d09bd9fd1b310ec251090c79af2d344
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2024-09-06 19:12:17 -0600

    fix(nvim): change sonokai style

--- nvim/lua/plugins/sonokai.lua
+++ nvim/lua/plugins/sonokai.lua
@@ -5,7 +5,7 @@ return {
       vim.g.sonokai_style = "andromeda"
       vim.g.sonokai_enable_italic = 1
       vim.g.sonokai_diagnostic_line_highlight = 1
-      vim.g.sonokai_disable_terminal_colors = 1
+      vim.g.sonokai_diagnostic_text_highlight = 1
       vim.g.sonokai_dim_inactive_windows = 1
       vim.g.sonokai_diagnostic_virtual_text = "colored"
     end,
