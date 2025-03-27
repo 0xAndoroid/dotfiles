9fe481bcf838f4303e9987ab4fe7622c77f34fec
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2025-03-27 12:06:39 -0400

    fix(sketchybar): constant width
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- sketchybar/plugins/cpu.sh
+++ sketchybar/plugins/cpu.sh
@@ -2,9 +2,14 @@
 
 CORE_COUNT=$(sysctl -n machdep.cpu.thread_count)
 CPU_INFO=$(ps -eo pcpu,user)
-CPU_SYS=$(echo "$CPU_INFO" | grep -v $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
-CPU_USER=$(echo "$CPU_INFO" | grep $(whoami) | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+CPU_SYS=$(echo "$CPU_INFO" | grep -v "$(whoami)" | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
+CPU_USER=$(echo "$CPU_INFO" | grep "$(whoami)" | sed "s/[^ 0-9\.]//g" | awk "{sum+=\$1} END {print sum/(100.0 * $CORE_COUNT)}")
 
 CPU_PERCENT="$(echo "$CPU_SYS $CPU_USER" | awk '{printf "%.0f\n", ($1 + $2)*100}')"
 
-sketchybar --set $NAME label="$CPU_PERCENT%"
+# If CPU PERCENT is single digit, add a space
+if [ "${#CPU_PERCENT}" -eq 1 ]; then
+  CPU_PERCENT="0$CPU_PERCENT"
+fi
+
+sketchybar --set "$NAME" label="$CPU_PERCENT%"
