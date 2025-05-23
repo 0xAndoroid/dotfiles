dbf4843090e395db705f94470b4f05aaa9cff04b
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2025-05-23 09:29:10 -0400

    fix(nvim): codeium works now
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,38 +1,38 @@
 return {
-  -- "Exafunction/windsurf.nvim",
-  -- event = "User AstroFile",
-  -- config = function() require("codeium").setup {
-  --   enable_chat = false,
-  --   enable_cmp_source = false,
-  --   virtual_text = {
-  --     enabled = true,
-  --     key_bindings = {
-  --       accept = "<C-t>",
-  --       next = "<M-]>",
-  --       prev = "<M-[>",
-  --     },
-  --   },
-  -- } end,
-  -- dependencies = {
-  --   {
-  --     "AstroNvim/astroui",
-  --     ---@type AstroUIOpts
-  --     opts = {
-  --       icons = {
-  --         Codeium = "",
-  --       },
-  --     },
-  --   },
-  -- },
-  -- specs = {
-  --   -- {
-  --   --   "onsails/lspkind.nvim",
-  --   --   optional = true,
-  --   --   -- Adds icon for codeium using lspkind
-  --   --   opts = function(_, opts)
-  --   --     if not opts.symbol_map then opts.symbol_map = {} end
-  --   --     opts.symbol_map.Codeium = require("astroui").get_icon("Codeium", 1, true)
-  --   --   end,
-  --   -- },
-  -- },
+  "Exafunction/windsurf.nvim",
+  event = "User AstroFile",
+  config = function() require("codeium").setup {
+    enable_chat = false,
+    enable_cmp_source = false,
+    virtual_text = {
+      enabled = true,
+      key_bindings = {
+        accept = "<C-t>",
+        next = "<M-]>",
+        prev = "<M-[>",
+      },
+    },
+  } end,
+  dependencies = {
+    {
+      "AstroNvim/astroui",
+      ---@type AstroUIOpts
+      opts = {
+        icons = {
+          Codeium = "",
+        },
+      },
+    },
+  },
+  specs = {
+    -- {
+    --   "onsails/lspkind.nvim",
+    --   optional = true,
+    --   -- Adds icon for codeium using lspkind
+    --   opts = function(_, opts)
+    --     if not opts.symbol_map then opts.symbol_map = {} end
+    --     opts.symbol_map.Codeium = require("astroui").get_icon("Codeium", 1, true)
+    --   end,
+    -- },
+  },
 }
