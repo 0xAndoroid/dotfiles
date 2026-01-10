f2060ae253c375616239c9343a653ca94d7aa101
Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>
2026-01-10 14:44:53 -0500

    feat(claude): claude-hud config
    Signed-off-by: Andrew Tretyakov <42178850+0xAndoroid@users.noreply.github.com>

--- create_links.sh
+++ create_links.sh
@@ -44,6 +44,7 @@ create_link ~/.dotfiles/alacritty ~/.config/alacritty
 create_link ~/.dotfiles/ghostty ~/.config/ghostty
 create_link ~/.dotfiles/claude/commands ~/.claude/commands
 create_link ~/.dotfiles/claude/rules ~/.claude/rules
+create_link ~/.dotfiles/claude/claude-hud ~/.claude/plugins/claude-hud
 create_link ~/.dotfiles/git-hooks ~/.git-hooks
 create_link ~/.dotfiles/wallpapers ~/Pictures/wallpaper
 create_link ~/.dotfiles/zellij ~/.config/zellij
